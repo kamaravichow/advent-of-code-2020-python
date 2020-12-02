f40dfbeba9caf0c1ac0dfeceda843ae80fe242b9
Aravind Chowdary <kamaravichow@gmail.com>
2020-12-02 23:50:47 +0530

    ✔️ Day 2- Part 1 & 2 solved

--- day2/part1/solution.py
+++ day2/part1/solution.py
@@ -8,16 +8,21 @@ correct_passwords_count = 0
 
 for item in cleaned_list:
     pCurrent = item.split(" ")
+
     pInfo = pCurrent[0]
+
     pTimesMin, pTimesMax = pInfo.split ("-")
+
     pLetter = pCurrent[1].strip(":")
+
     pCheckString = pCurrent[2].strip()
 
     # 1 letter should be atleast ptimesmin
     # 2 letter count should not exceed ptimesmax
     pCurrentLetterCount = pCheckString.count(pLetter)
+
     if(pCurrentLetterCount >= int(pTimesMin) and pCurrentLetterCount <= int(pTimesMax)):
-        correct_passwords_count = correct_passwords_count + 1
+        correct_passwords_count += 1
 
 
 print(correct_passwords_count)
